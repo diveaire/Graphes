4e98e972de49c926c2b99046228c6972dfe776ca
Nicolas <n.auvray.na@gmail.com>
2024-01-17 11:04:48 +0100

    Mise à jour du commentaire

--- src/graphe.c
+++ src/graphe.c
@@ -2,9 +2,10 @@
 #include "header/graphe.h"
 
 /*
-Sérialization : 
-param------------------
-(0,1,a);arc2;arc3
+    * Implementation des graphes représentation par matrice d'adjacences
+    * Sérialization : 
+        (1èreLigne) paramètres
+        (0,1,a);arc2;arc3
 */
 
 int main(int argc, char * argv[]){
